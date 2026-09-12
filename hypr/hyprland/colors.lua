c395d497cc552c02288cd824b377921de66a9f84
Hope <169930374+HopeX23@users.noreply.github.com>
2026-09-12 21:56:22 +0530

    Add initial hyprland and kitty configurations
    Signed-off-by: Hope <169930374+HopeX23@users.noreply.github.com>

---
+++ hypr/hyprland/colors.lua
@@ -0,0 +1,16 @@
+hl.config({
+    general = {
+        col = {
+            active_border   = "rgba(46474777)",
+            inactive_border = "rgba(1a1c1c33)",
+        },
+    },
+    misc = {
+        background_color = "rgba(121414FF)",
+    },
+})
+
+hl.window_rule({
+    match        = { pin = 1 },
+    border_color = "rgba(b8cacaAA) rgba(b8caca77)",
+})
